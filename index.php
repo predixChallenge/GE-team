1451d3a120d21a2b2d384d4639ad1c59abe47aa4
Ronald Nettey <ronaldnettey@yahoo.com>
2017-03-14 00:22:04 +0000

    Data UI created

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+$distance = $_GET['distance'];
+$state = $_GET['state'];
+$percentage = $_GET['percentage'];
+$timestamp = $_GET['timestamp'];
+
+echo 'Distance : '."$distance" . "<br>";
+echo 'State : '."$state" . "<br>";
+echo 'Percentage : '."$percentage" . "<br>";
+echo 'Time Stamp : '."$timestamp" . "<br>";
+
+
+?>
